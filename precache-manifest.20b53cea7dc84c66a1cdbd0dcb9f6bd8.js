35b6693ea236e2b430e614eada62107cc1d03c53
Ezequiel <ezequiel@pop-os.localdomain>
2020-08-01 19:40:07 -0300

    Updates

---
+++ precache-manifest.20b53cea7dc84c66a1cdbd0dcb9f6bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76edf6a7ecd794de5baed32383e3ec5a",
+    "url": "/reactjs-tutorial/index.html"
+  },
+  {
+    "revision": "5a284ccf98f65c74c04a",
+    "url": "/reactjs-tutorial/static/css/main.aae3cbd3.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/reactjs-tutorial/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-tutorial/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a284ccf98f65c74c04a",
+    "url": "/reactjs-tutorial/static/js/main.050138cf.chunk.js"
+  },
+  {
+    "revision": "51a06d9dca623c2bf0e8",
+    "url": "/reactjs-tutorial/static/js/runtime-main.f537dd61.js"
+  }
+]);
